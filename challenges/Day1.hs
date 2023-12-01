4b3552ce20016f23ca2d008f7ac81d8be39727c9
bkrmendy <korber314@gmail.com>
2023-12-01 08:11:19 +0100

    moar cleanup

--- challenges/Day1.hs
+++ challenges/Day1.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Advent (challenge)
 import Data.Char (isDigit, digitToInt)
-import Data.List (isPrefixOf)
+import Data.List (isPrefixOf, tails)
 
 type Challenge = [String]
 
@@ -15,21 +15,16 @@ p digits = 10 * (head digits) + last digits
 part1 :: Challenge -> Int
 part1 = sum . map (p . map digitToInt . filter isDigit)
 
-matchNumber :: String -> [Int]
-matchNumber value = [n | (literal, n) <- numbers, isPrefixOf literal value]
-  where
-     numbers = [ ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)
-               , ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)
-               ]
+numbers :: [(String, Int)]
+numbers = [ ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)
+         , ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)
+         ]
 
 part2 :: Challenge -> Int
 part2 = sum . map (p . calibrationValue)
   where
-        calibrationValue :: String -> [Int]
-        calibrationValue [] = []
-        calibrationValue value = case matchNumber value of
-          [number] -> (number:calibrationValue (tail value))
-          _ -> calibrationValue (tail value)
+    calibrationValue :: String -> [Int]
+    calibrationValue line = [n | value <- tails line, (literal, n) <- numbers, isPrefixOf literal value]
 
 main :: IO ()
 main = challenge "1" parse part1 part2
