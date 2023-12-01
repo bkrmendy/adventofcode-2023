10f28177ab3e98541d167bcd24af1e935241fd12
bkrmendy <korber314@gmail.com>
2023-12-01 08:16:41 +0100

    moar cleanup

--- challenges/Day1.hs
+++ challenges/Day1.hs
@@ -9,22 +9,22 @@ type Challenge = [String]
 parse :: String -> Challenge
 parse = lines
 
-p :: [Int] -> Int
-p digits = 10 * (head digits) + last digits
+calibrationValue :: [Int] -> Int
+calibrationValue digits = 10 * (head digits) + last digits
 
 part1 :: Challenge -> Int
-part1 = sum . map (p . map digitToInt . filter isDigit)
+part1 = sum . map (calibrationValue . map digitToInt . filter isDigit)
 
-numbers :: [(String, Int)]
-numbers = [ ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)
+patterns :: [(String, Int)]
+patterns = [ ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)
          , ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)
          ]
 
 part2 :: Challenge -> Int
-part2 = sum . map (p . calibrationValue)
+part2 = sum . map (calibrationValue . numbers)
   where
-    calibrationValue :: String -> [Int]
-    calibrationValue line = [n | value <- tails line, (literal, n) <- numbers, isPrefixOf literal value]
+    numbers :: String -> [Int]
+    numbers line = [n | value <- tails line, (literal, n) <- patterns, isPrefixOf literal value]
 
 main :: IO ()
 main = challenge "1" parse part1 part2
