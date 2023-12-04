b5ae088a7412a483dc73d63b568c3b27a3ed2a3f
bkrmendy <korber314@gmail.com>
2023-12-04 09:43:07 +0100

    arrays

--- challenges/Day4.hs
+++ challenges/Day4.hs
@@ -1,9 +1,11 @@
 module Main where
 
 import qualified Data.HashSet as S
-import qualified Data.HashMap.Strict as M
 import qualified Text.Parsec as P
-import Data.List (foldl')
+import qualified Data.Array.MArray as A
+import Data.Array.ST (STArray)
+import Control.Monad
+import Control.Monad.ST
 import Advent (challenge)
 import Utils (int, parseLines)
 
@@ -22,25 +24,21 @@ parse = map (parseLines card) . lines
 score :: Card -> Int
 score (MkCard _ winning valid) = S.size $ S.intersection winning valid
 
-value :: Int -> Int
-value 0 = 0
-value n = 2 ^ (n - 1)
-
 part1 :: Challenge -> Int
-part1 = sum . map (value . score)
-
-type Counts = M.HashMap Int Int
-
-update :: Counts -> Int -> [Int] -> Counts
-update cs i is = foldl' go cs is
-  where go cs' ix = M.insertWith (+) (i + ix) (cs M.! i) cs'
+part1 cards = sum $ [2 ^ (s - 1) | c <- cards, let s = score c, s > 0]
 
 part2 :: Challenge -> Int
-part2 cards = sum $ foldl' go counts $ [(i, score c) | (i, c) <- zip [1..] cards]
+part2 cards = runST $ do
+  arr <- A.newArray (1, length scores) 1 :: ST s (STArray s Int Int)
+  forM_ scores $ \(i, s) -> do
+    forM_ [i+1..i+s] $ \ix -> do
+      v1 <- A.readArray arr i
+      v2 <- A.readArray arr ix
+      A.writeArray arr ix (v1 + v2)
+  es <- A.getElems arr
+  return $ sum es
   where
-    counts = M.fromList [(i, 1) | (MkCard i _ _) <- cards]
-    go :: Counts -> (Int, Int) -> Counts
-    go cs (i, ms) = update cs i [1..ms]
+    scores = zip [1..] $ map score cards
 
 main :: IO ()
 main = challenge "4" parse part1 part2
