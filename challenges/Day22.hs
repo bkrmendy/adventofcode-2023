b6b0e0c36a9dfb0c11ebbf89f4a788dc9a5f5abf
bkrmendy <korber314@gmail.com>
2023-12-22 16:28:52 +0100

    d22 - p1

--- challenges/Day22.hs
+++ challenges/Day22.hs
@@ -2,49 +2,51 @@ module Main where
 
 import qualified Data.HashMap.Strict as M
 import qualified Data.HashSet as S
-import Data.List (sortOn, foldl')
+import Data.List (sort, sortOn, nub, delete)
 import Data.List.Split (splitOn)
 import Advent (visual)
 import Utils (readInt)
 import Debug.Trace (traceShow)
 
-type Piece = (Int, (Int, Int, Int), (Int, Int, Int))
+type Piece = ((Int, Int), (Int, Int), (Int, Int))
 type Challenge = [Piece]
 
 parse :: String -> Challenge
-parse = sortOn (\(_, (_, _, fz), (_, _, tz)) -> (fz, tz)) . map (\(s, line) -> p s $ splitOn "~" line) . zip [1..] . lines
-  where p s [f, t] = (s, c (splitOn "," f), c (splitOn "," t))
-        c [x, y, z] = (readInt x, readInt y, readInt z)
-
-type FallState = M.HashMap (Int, Int) (Int, Int) -- piece id, height
-type PieceDependency = (Int, [Int]) -- piece id, rests on pieces
-
-supportingCubes :: FallState -> [(Int, Int)] -> (Int, [Int])
-supportingCubes state footprint = (maxZ, supports)
-  where zs = [M.lookupDefault (0, 0) (x, y) state | (x, y) <- footprint]
-        maxZ = maximum (map snd zs)
-        supports = [i | (i, z) <- zs, z == maxZ]
-
-fall :: FallState -> [Piece] -> [PieceDependency]
-fall _ [] = []
-fall state (piece:rest)= (s, supports):fall nextState rest
+parse = sortOn (\(_, _, z) -> z) . map (p . splitOn "~") . lines
   where
-    (s, (fx, fy, fz), (tx, ty, tz)) = piece
-    height = length [fz..tz]
-    footprint = [(x, y) | x <- [fx..tx], y <- [fy..ty]]
-    (maxZFromSupports, supports) = supportingCubes state footprint
-    bottomZ = maxZFromSupports + 1
-    topZ = bottomZ + height
-    nextState = foldl' (\m c -> M.insert c (s, topZ) m) state footprint
+    p [f, t] =
+      let
+        ([fx, fy, fz], [tx, ty, tz]) = ((splitOn "," f), (splitOn "," t))
+      in ((readInt fx, readInt tx), (readInt fy, readInt ty), (readInt fz, readInt tz))
 
-count :: [PieceDependency] -> Int
-count deps = length piecesToRemove
+intersects :: (Int, Int) -> (Int, Int) -> Bool
+intersects a b = let [(l1, h1), (l2, h2)] = sort [a, b] in l1 <= h2 && l2 <= h1
+
+footprintsOverlap :: Piece -> Piece -> Bool
+footprintsOverlap (x, y, _) (xx, yy, _) = intersects x xx && intersects y yy
+
+fall :: [Piece] -> [Piece] -> [Piece]
+fall acc [] = acc
+fall acc (piece:rest) = fall ((go acc piece 1):acc) rest
   where
-    singleSupportPieces = S.fromList [i | [i] <- map snd deps, i /= 0]
-    piecesToRemove = [i | i <- map fst deps, not (S.member i singleSupportPieces), i /= 0]
+    go [] (x, y, (lz, hz)) maxZ = let drp = lz - maxZ in (x, y, (lz - drp, hz - drp))
+    go (a@(_, _, az):aa) p maxZ
+      | footprintsOverlap p a = go aa p (max maxZ (snd az + 1))
+      | otherwise = go aa p maxZ
+
+isAbove :: Piece -> Piece -> Bool
+(_, _, (lz, _)) `isAbove` (_, _, (_, hz)) = lz == hz + 1
+
+supporters :: [Piece] -> M.HashMap Int [Piece]
+supporters pieces = M.fromList $ map go (zip [0..] $ pieces)
+  where go (i, p) = (i, [n | n <- pieces, footprintsOverlap n p, p `isAbove` n])
+
+count :: M.HashMap Int [Piece] -> Int
+count sps = M.size sps - length singleSupports
+  where singleSupports = nub $ [p | [p] <- M.elems sps]
 
 part1 :: Challenge -> String
-part1 = show . count . fall M.empty
+part1 = show . count . supporters . fall []
 
 part2 :: Challenge -> String
 part2 = const "-"
